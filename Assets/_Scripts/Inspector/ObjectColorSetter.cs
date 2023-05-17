eb11d1cb081ab8508a3a8c4481adf28e0d64a43d
yashwankhade <yashwankhade987@gmail.com>
2023-05-17 23:33:51 +0530

    Made script to work with multiple render pipleline

--- Assets/_Scripts/Inspector/ObjectColorSetter.cs
+++ Assets/_Scripts/Inspector/ObjectColorSetter.cs
@@ -1,33 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-
+public enum RenderPipelineType
+{
+    Default,
+    Universal,
+    HDRP
+}
 //This class helps to change material color without changing assest from inspector
 public class ObjectColorSetter : MonoBehaviour
 {
     MaterialPropertyBlock mpb;
-
-    MaterialPropertyBlock Mpb 
+    [Tooltip("Which type of renderpipeline is used by the project")]
+    [SerializeField] RenderPipelineType projectPipelineType;
+    MaterialPropertyBlock Mpb
     {
 
-        get 
+        get
         {
             if (mpb == null)
                 mpb = new MaterialPropertyBlock();
             return mpb;
         }
     }
+
     public Color m_Color;
     private void OnValidate()
     {
-        ApplyColor();   
+        ApplyColor();
     }
 
-    void ApplyColor() 
+    void ApplyColor()
     {
-        Renderer rnd = GetComponent<Renderer>();
-        Mpb.SetColor("_BaseColor", m_Color);
+        MeshRenderer rnd = GetComponent<MeshRenderer>();
+
+        switch (projectPipelineType)
+        {
+            case RenderPipelineType.Default:
+                Mpb.SetColor("_Color", m_Color);
+                break;
+            case RenderPipelineType.Universal:
+                Mpb.SetColor("_BaseColor", m_Color);
+                break;
+            case RenderPipelineType.HDRP:
+                Mpb.SetColor("_BaseColor", m_Color);
+                break;
+        }
         rnd.SetPropertyBlock(Mpb);
     }
 }
