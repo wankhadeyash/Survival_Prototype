fa33ecbe93fdf8f17a2829ac4b48bfb127b64952
yashwankhade <yashwankhade987@gmail.com>
2023-06-03 14:11:41 +0530

    Custom scene manager added

---
+++ Assets/_Scripts/CustomSceneManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+
+public class CustomSceneManager : SingletonBase<CustomSceneManager>
+{
+    public static void LoadScene(string sceneName, Action OnSceneLoaded = null)
+    {
+        Instance.LoadSceneInternal(sceneName, OnSceneLoaded);
+    }
+
+    private void LoadSceneInternal(string sceneName, Action OnSceneLoaded)
+    {
+        StartCoroutine(LoadSceneAsync(sceneName, OnSceneLoaded));
+    }
+
+    private System.Collections.IEnumerator LoadSceneAsync(string sceneName, Action OnSceneLoaded)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        OnSceneLoaded?.Invoke();
+    }
+
+
+    public static void LoadScene(int sceneIndex, Action OnSceneLoaded = null)
+    {
+        Instance.LoadSceneInternal(sceneIndex, OnSceneLoaded);
+    }
+
+    private void LoadSceneInternal(int sceneIndex, Action OnSceneLoaded)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex, OnSceneLoaded));
+    }
+
+    private System.Collections.IEnumerator LoadSceneAsync(int sceneIndex, Action OnSceneLoaded)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        OnSceneLoaded?.Invoke();
+    }
+    public static void RestartScene(Action onSceneLoaded = null)
+    {
+        Instance.RestartSceneInternal(onSceneLoaded);
+    }
+
+    private void RestartSceneInternal(Action onSceneLoaded)
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        LoadScene(currentScene.name, onSceneLoaded);
+    }
+}
