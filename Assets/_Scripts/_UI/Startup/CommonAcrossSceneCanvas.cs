982afffbaaee4a5f005753650851e6128fe83215
yashwankhade <yashwankhade987@gmail.com>
2023-06-30 23:04:39 +0530

    asd

---
+++ Assets/_Scripts/_UI/Startup/CommonAcrossSceneCanvas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UIToolkitDemo
+{
+    public class CommonAcrossSceneCanvas : MonoBehaviour
+    {
+        void Awake()
+        {
+            
+        }
+
+        void Start()
+        {
+            
+        }
+
+    }
+}
---
+++ Assets/_Scripts/_UI/Startup/LoadingUI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class LoadingUI : MonoBehaviour
+{
+
+    [SerializeField] GameObject m_Container;
+    [SerializeField] TextMeshProUGUI m_LoadingText;
+    bool m_IsEnabled;
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        Show("Connecting to unity");
+    }
+
+    public void Show(string message)
+    {
+        m_LoadingText.text = message;
+        m_Container.SetActive(true);
+        m_IsEnabled = true;
+        StartCoroutine(Co_Animate());
+    }
+
+    public void Hide() 
+    {
+        m_LoadingText.text = "";
+        m_Container.SetActive(false);
+        m_IsEnabled = false;
+
+    }
+
+    private IEnumerator Co_Animate() 
+    {
+        string originalText = m_LoadingText.text;
+        while (m_IsEnabled) 
+        {
+            m_LoadingText.text = originalText + ".";
+            yield return new WaitForSecondsRealtime(0.25f);
+            m_LoadingText.text = originalText + "..";
+            yield return new WaitForSecondsRealtime(0.25f);
+            m_LoadingText.text = originalText + "...";
+            yield return new WaitForSecondsRealtime(0.25f);
+            m_LoadingText.text = originalText;
+
+        }
+    }
+
+}
+
